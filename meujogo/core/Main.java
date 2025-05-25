030db37645ce6d52fb12251d2a0f9ec4eab4b8c6
Eduardo Nunziata <eduardonunziata@outlook.com>
2025-05-25 13:17:10 -0300

    Update Main.java
    Comentários adicionados.

--- meujogo/core/Main.java
+++ meujogo/core/Main.java
@@ -1,8 +1,20 @@
+// Pacote onde a classe está localizada
 package meujogo.core;
 
+/**
+ * Classe principal que inicia o jogo.
+ */
 public class Main {
+
+    /**
+     * Método main: ponto de entrada da aplicação.
+     * Quando o programa é executado, ele começa por aqui.
+     */
     public static void main(String[] args) {
+        // Cria uma instância do controlador do jogo
         GameController game = new GameController();
+
+        // Inicia o jogo chamando o método startGame()
         game.startGame();
     }
 }
